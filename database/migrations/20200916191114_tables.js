dfca1e9aa3463aa486436bb3477a8f8878ef317b
Jose Paquian <alostnight1993@outlook.com>
2020-09-28 17:16:07 -0500

    fixes

--- database/migrations/20200916191114_tables.js
+++ database/migrations/20200916191114_tables.js
@@ -24,5 +24,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("posts").dropTableIfExists("users");
 };
